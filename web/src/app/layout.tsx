b898ebc995de904c9db1e059b5602a5a19e2c8f6
akanthed <akshay.kanthed007@gmail.com>
2026-05-15 19:20:04 +0530

    Fix issues

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -58,8 +58,8 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      maxImagePreview: "large",
-      maxSnippet: -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   alternates: {
